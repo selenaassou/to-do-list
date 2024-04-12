8a2a1351115857032f59bbfc0406d97c9008086e
selenaassou <aselenateaches@gmail.com>
2024-04-12 14:51:23 +0000

    UPDATED CSS AND JS FILES. WOW NO NEED FOR HTML NOW. WE CAN DO SO MUCH TO ONE HTML TAG AMAZING.

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 // see this awesome reference : https://www.freecodecamp.org/news/innerhtml-vs-innertext-vs-textcontent/
-// Function after clicking the button: if empty: tell uer to enter text, otherwise, read input as html & add to the list.
+// Function after clicking the button: if empty: tell uer to enter text, otherwise, read input as html & add/display to the list.
+// We also want to clear the input box & remove duplicate entreid (me)
 function addTask(){
     if(inputBox.value == ''){
         alert("You must write something!");
@@ -15,5 +16,22 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        // The <span> tag is an INLINE container used to mark up a part of a text, or a part of a document
+        // this line: span.innerHTML = "\u00d7"; the code is for "cross sumbol x" \. Now needs CSS styling!
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span); 
     }
+    inputBox.value = "";
 }
+
+// Action when Task is clicked. Also, Action when clicking the cross x to delete the Task.
+// Notice ALL THE CODE is inside the adEventListener function PARENTHESES!
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
